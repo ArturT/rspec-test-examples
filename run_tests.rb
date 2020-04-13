d393d07450bfca1384215bfdbd9d562d4f2ae820
Artur Trzop <arturtrzop@gmail.com>
2020-04-13 16:49:22 +0200

    Update run_tests.rb

--- run_tests.rb
+++ run_tests.rb
@@ -7,13 +7,14 @@ def start_runner(cli_args)
   args = ['--format', 'documentation'] + cli_args
   options = RSpec::Core::ConfigurationOptions.new(args)
   RSpec::Core::Runner.new(options).run($stderr, $stdout)
+
+  RSpec.clear_examples
 end
 
 
 cli_args = ['spec/example_spec.rb[1:1]', 'spec/example2_spec.rb']
 start_runner(cli_args)
 
-RSpec.clear_examples
-
 cli_args = ['spec/example_spec.rb[1:2]', 'spec/example3_spec.rb']
 start_runner(cli_args)
+
